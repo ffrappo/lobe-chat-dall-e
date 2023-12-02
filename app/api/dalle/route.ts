dc844dffa043b23b5cfe483a27fbaf5f222de8de
frapporti <49104678+frapporti@users.noreply.github.com>
2023-12-02 11:08:41 +0100

    Update route.ts

--- app/api/dalle/route.ts
+++ app/api/dalle/route.ts
@@ -1,32 +1,36 @@
-// api/dalle/route.ts
+// app/api/dalle/route.ts
+
 import { NextRequest, NextResponse } from 'next/server';
-import { OpenAI } from 'openai';
-import { serialize } from 'v8';
+import OpenAI from 'openai';
 
-export const runtime = "experimental-edge"
-
-export default async function handler(request: NextRequest) {
-    console.log({request})
-    if (request.method !== 'POST') {
-        return new NextResponse(null, { status: 405, statusText: 'Method Not Allowed' + serialize(request) });
+export default async function handler(req: NextRequest) {
+    if (req.method !== 'POST') {
+        return new NextResponse('Method Not Allowed', { status: 405 });
     }
 
-    const prompt = await request.json();
-
+    const body = await req.json();
     const openai = new OpenAI();
 
     try {
-        const response = await openai.images.generate({
+        const imageResponse = await openai.images.generate({
             model: "dall-e-3",
-            prompt: prompt.message,
-            n: 1,
-            size: "1024x1024",
-            quality: 'hd',
+            prompt: body.message
         });
-        const image_url = response.data[0].url;
 
-        return NextResponse.json({ image_url });
-    } catch (error: any) {
-        return new NextResponse(JSON.stringify({ error: error.message }), { status: 500 });
+        const image_url = imageResponse.data[0].url;
+
+        return new NextResponse(JSON.stringify({ image_url }), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    } catch (error) {
+        return new NextResponse(JSON.stringify({ error: error.message }), {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
     }
 }
