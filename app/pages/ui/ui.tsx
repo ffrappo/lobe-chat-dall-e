dffb00c54a4cb6aaf4c0f91f03ae8d5430df0620
frapporti <49104678+frapporti@users.noreply.github.com>
2023-12-02 12:37:19 +0100

    Update ui.tsx

--- app/pages/ui/ui.tsx
+++ app/pages/ui/ui.tsx
@@ -1,18 +1,22 @@
 import { useWatchPluginMessage } from '@lobehub/chat-plugin-sdk/client';
+import { memo, useEffect, useState } from 'react';
 
-const Daje = () => {
+const Daje = memo(() => {
   const { data, loading } = useWatchPluginMessage();
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
+  console.log({data})
+
   return (
     <div>
       <h1>Plugin Sent Message Data:</h1>
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      <img src={data} />
     </div>
   );
-};
+});
 
 export default Daje;
