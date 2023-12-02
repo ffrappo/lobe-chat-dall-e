44cac33d3cffe7a28a1e6ed1d5d60e4e7d0826dd
frapporti <49104678+frapporti@users.noreply.github.com>
2023-12-02 13:39:41 +0100

    icon

--- app/ui/page.tsx
+++ app/ui/page.tsx
@@ -1,13 +1,16 @@
-'use client'
+'use client';
 import { useWatchPluginMessage } from '@lobehub/chat-plugin-sdk/client';
 import { memo } from 'react';
+import { LinkIcon } from '@heroicons/react/20/solid';
 
 const Daje = memo(() => {
   const { data, loading } = useWatchPluginMessage();
+  const placeholderImage = 'https://user-images.githubusercontent.com/20684618/31289519-9ebdbe1a-aae6-11e7-8f82-bf794fdd9d1a.png';
+  const imageSrc = data ? data.image_url : placeholderImage;
 
   if (loading) {
     return (
-      <div className="p-4 max-w-sm mx-auto">
+      <div className="container mx-auto p-4">
         <div className="animate-pulse flex flex-col items-center space-y-4">
           <div className="bg-gray-300 h-6 w-3/4 rounded"></div>
           <div className="h-60 w-full bg-gray-300 rounded"></div>
@@ -17,19 +20,21 @@ const Daje = memo(() => {
   }
 
   return (
-    <div className="p-4 max-w-sm mx-auto">
+    <div className="container mx-auto p-4">
       <div className="bg-white shadow-xl rounded-lg overflow-hidden">
-        <div className="p-4">
-          <h1 className="text-xl font-bold text-gray-800 mb-3">Generated Image</h1>
-        </div>
         <div>
-          <a href={data ? data.image_url : 'https://user-images.githubusercontent.com/20684618/31289519-9ebdbe1a-aae6-11e7-8f82-bf794fdd9d1a.png'}
-             target="_blank" rel="noopener noreferrer">
-            <img className="w-full h-60 object-cover rounded"
-                 src={data ? data.image_url : 'https://user-images.githubusercontent.com/20684618/31289519-9ebdbe1a-aae6-11e7-8f82-bf794fdd9d1a.png'}
+          <a href={imageSrc} target="_blank" rel="noopener noreferrer">
+            <img className="w-full max-h-75 object-cover rounded-t-lg"
+                 src={imageSrc}
                  alt="Generated" 
-                 style={{height: 300}}
+                 style={{ maxHeight: '300px' }}
                  />
+          </a>
+        </div>
+        <div className="p-4">
+          <a href={imageSrc} target="_blank" rel="noopener noreferrer" className="flex items-center text-sm text-gray-500 hover:text-gray-800">
+            <span>View Image</span>
+            <LinkIcon className="ml-1 w-4 h-4" />
           </a>
         </div>
       </div>
